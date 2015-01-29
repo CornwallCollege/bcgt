a0068201232b12b42a964aab3249cc863cc8db33
jfrickard <john.rickard@cornwall.ac.uk>
2015-01-29 07:11:48 +0000

    v7_4 - amended version.php to push upgrade

--- version.php
+++ version.php
@@ -2,7 +2,7 @@
 
 //Each time the upgrade process is to be run this should be the date of the day when the 
 //code and database changes are made!!!!!!
-    $plugin->version = 2015010800;  // YYYYMMDDHH (year, month, day, 24-hr time)
+    $plugin->version = 2015010801;  // YYYYMMDDHH (year, month, day, 24-hr time)
     $plugin->requires = 2012120300; // Moodle 2.4
     //$plugin->cron = 60; - This would be every 60 seconds..................
     $plugin->cron = 3600;
